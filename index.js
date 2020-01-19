671e387e84e382af6bfc97a9b77def496715e0a7
Oliver Beardsall <oli@beardsall.eu>
2020-01-19 16:29:44 +0000

    I'm a moron v2

--- index.js
+++ index.js
@@ -29,11 +29,9 @@ client.on('message', msg => {
     msg.channel.send("You're an egg.")
     msg.channel.send("🥚🥚🥚")
   }
+})
 client.on('message', msg => {
   if (msg.content === 'wat') {
-    //const attachment = new MessageAttachment('https://bubcool1.github.io/wat/wat.jpg');
-    // Send the attachment in the message channel
-    //msg.channel.send(attachment);
     message.channel.send('wat', {
       files: [
           "./wat.jpg"
