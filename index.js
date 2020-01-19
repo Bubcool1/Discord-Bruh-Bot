1735baaa45a4c673462867a4c59280fa26e6dbf6
Oliver Beardsall <oli@beardsall.eu>
2020-01-19 16:40:07 +0000

    It works, time to break it again.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('message', msg => {
   }
 })
 client.on('message', msg => {
-  if (msg.content === '!wat') {
+  if (msg.content === 'wat') {
     const attachment = new MessageAttachment('https://bubcool1.github.io/wat/wat.jpg');
 		// Send the attachment in the message channel
 		msg.channel.send(attachment);
