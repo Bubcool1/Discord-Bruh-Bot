007c7cdca80efb244a0935fe4337a0a44c7b3ce1
Oli <oli@beardsall.dev>
2021-07-22 16:02:53 +0100

    Mild optimisation.
    More optimisation and slash commands coming soon TM.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Client, MessageAttachment, Message } = require('discord.js');
+const {
+  Client,
+  MessageAttachment,
+  Message
+} = require('discord.js');
 const client = new Client();
 
 // Added for local testing
@@ -16,99 +20,95 @@ const db = admin.firestore();
 
 client.on('ready', () => {
   console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
-      client.user.setStatus('online')
-      client.user.setPresence({
-          game: {
-              name: 'FOR RESPONSES',
-              type: "Watching",
-              url: "http://obeardsall.media/wat/"
-          }
-      });
+  client.user.setStatus('online')
+  client.user.setPresence({
+    game: {
+      name: 'FOR RESPONSES',
+      type: "Watching",
+      url: "http://obeardsall.media/wat/"
+    }
   });
+});
 
 client.on('message', msg => {
   if (msg.content === '!bruh') {
-    msg.delete({ timeout: 1})
+    msg.delete({
+      timeout: 1
+    })
     msg.channel.send("Bruh Moment")
     msg.channel.send("<:bruh:629372877388382209> <:bruh:629372877388382209> <:bruh:629372877388382209>")
   }
-});
-client.on('message', msg => {
   if (msg.content === '🥚') {
     msg.channel.send("You're an egg.")
     msg.channel.send("🥚🥚🥚")
   }
-})
-client.on('message', msg => {
   if (msg.content === "<:EJ:629372599432118292>") {
     msg.channel.send("AH IT'S MUFFIN TIME <:squarenitin:632113566874599444>")
   }
-})
-client.on('message', msg => {
   if (msg.content === '!egg') {
     msg.channel.send("You're an egg.")
     msg.channel.send("🥚🥚🥚")
   }
-})
-client.on('message', msg => {
   if (msg.content.toLowerCase() === 'wat') {
-    msg.channel.send(" ", {files: ["assets/wat.jpg"]});
+    msg.channel.send(" ", {
+      files: ["assets/wat.jpg"]
+    });
   }
-})
 
-client.on('message', msg => {
   if (msg.content === '!goosegif') {
-    msg.delete({ timeout: 1})
+    msg.delete({
+      timeout: 1
+    })
     msg.channel.send("HONK HONK HONK")
     const attachment = new MessageAttachment('assets/GooseDance.gif');
     msg.channel.send(attachment);
   }
-})
-client.on('message', msg => {
   if (msg.content === '!honks') {
-    msg.delete({ timeout: 1})
+    msg.delete({
+      timeout: 1
+    })
     msg.channel.send("HONK HONK HONK")
-    msg.channel.send(" ", {files: ["assets/GooseHonks.png"]});
+    msg.channel.send(" ", {
+      files: ["assets/GooseHonks.png"]
+    });
   }
-})
-client.on('message', msg => {
   if (msg.content === '!badgoose') {
-    msg.delete({ timeout: 1})
+    msg.delete({
+      timeout: 1
+    })
     msg.channel.send("HONK HONK HONK")
-    msg.channel.send(" ", {files: ["assets/GooseBonk.png"]});
+    msg.channel.send(" ", {
+      files: ["assets/GooseBonk.png"]
+    });
   }
-})
-client.on('message', msg => {
-  if(msg.content === "!oli"){
+  if (msg.content === "!oli") {
     msg.channel.send("Ha no.")
   }
-})
-client.on('message', msg => {
   if (msg.content.toLowerCase() === 'pls goose') {
     msg.channel.send("https://samperson.itch.io/desktop-goose?download")
   }
-})
 
-client.on('message', msg => {
-  if(msg.content.toLowerCase() === "pls goose mods"){
+  if (msg.content.toLowerCase() === "pls goose mods") {
     msg.channel.send("https://desktopgooseunofficial.github.io/ResourceHub/")
   }
-})
 
-client.on('message', msg => {
   if (msg.content === 'wot') {
-    msg.channel.send(" ", {files: ["assets/YOUWOTM9-1.jpg"]});
+    msg.channel.send(" ", {
+      files: ["assets/YOUWOTM9-1.jpg"]
+    });
   }
-})
-client.on('message', msg => {
   if (msg.content === 'Wot') {
-    msg.channel.send(" ", {files: ["assets/YOUWOTM9-2.jpg"]});
+    msg.channel.send(" ", {
+      files: ["assets/YOUWOTM9-2.jpg"]
+    });
   }
-})
-client.on('message', msg => {
   if (msg.content === '!shut') {
-    msg.delete({ timeout: 1})
-    msg.channel.send(" ", {files: ["assets/shut.png"]});
+    msg.delete({
+      timeout: 1
+    })
+    msg.channel.send(" ", {
+      files: ["assets/shut.png"]
+    });
   }
 });
 
@@ -140,27 +140,30 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() == 'noice') {
-    msg.delete({ timeout: 1})
-    msg.channel.send(" ", {files: ["assets/noice.gif"]})
+    msg.delete({
+      timeout: 1
+    })
+    msg.channel.send(" ", {
+      files: ["assets/noice.gif"]
+    })
   };
 });
 
 client.on('message', msg => {
   if (!msg.content.startsWith('!print') || msg.author.bot) return;
-    global.args = msg.content.slice(6).trim();
-    if(args == "") {
-        msg.channel.send("Please give a message.");
-    }
-    else {
-      var content = args + " " + msg.author.tag
-      console.log(content)
+  global.args = msg.content.slice(6).trim();
+  if (args == "") {
+    msg.channel.send("Please give a message.");
+  } else {
+    var content = args + " " + msg.author.tag
+    console.log(content)
 
-      db.collection('requests').doc('test').set({
-        printed: false,
-        text: content
-      });
-      console.log("Message sent:" + args)
-    }
+    db.collection('requests').doc('test').set({
+      printed: false,
+      text: content
+    });
+    console.log("Message sent:" + args)
+  }
 });
 
 
